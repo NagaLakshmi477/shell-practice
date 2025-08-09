4216212b7191ed2b37f363f56e6cde37823031e8
NagaLakshmi477 <jaishikareddy997@gmail.com>
2025-08-09 10:36:33 +0530

    git shell

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,6 +3,6 @@
 NAMES=("ramesh" "suresh" "mahesh")
 
 echo "First name is ${NAMES[0]}"
-# echo " second name is $NAMES[1]"
-# echo " Third name is $NAMES[2]"
-# echo " All names $NAMES[$@]"
+echo " second name is ${NAMES[1]}"
+echo " Third name is ${NAMES[2]}"
+echo " All names ${NAMES[$@]}"
