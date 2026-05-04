d16f3bc4fe2c2cccf5db6cdf9b72776bd776553d
NagaLakshmi477 <jaishikareddy997@gmail.com>
2026-05-04 09:24:16 +0530

    shell

---
+++ 03.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PER1: "Ram"
+PER2: "sita"
+
+echo " hello $PER1 "
+echo " Hi $PER2 "
+echo " how are you $PER1?"
+echo " i am fine $PER2. What about you?"
+echo " I am also good $PER1. "
