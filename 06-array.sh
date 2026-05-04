84046fa11b0feabcb121606d2e2ed3055adc3b8a
NagaLakshmi477 <jaishikareddy997@gmail.com>
2026-05-04 10:45:17 +0530

    shell

--- 06-array.sh
+++ 06-array.sh
@@ -3,5 +3,5 @@
 NAMES=("ram" "sita" "suresh" "ramesh")
 
 echo "1st name of the person is : ${NAMES[0]}"
-echo " 2nd name of the person is : ${NAMES[1]}
-echo " 3rd name of the person is : ${NAMES[2]}
+echo " 2nd name of the person is : ${NAMES[1]}"
+echo " 3rd name of the person is : ${NAMES[2]}"
